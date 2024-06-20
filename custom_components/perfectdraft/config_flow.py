ac0ae11a28e3dd13593989d45ca019bbd4ad7926
matthewcky2k <matthew@luckins.im>
2024-06-20 15:08:11 +0100

    Update config_flow.py

--- custom_components/perfectdraft/config_flow.py
+++ custom_components/perfectdraft/config_flow.py
@@ -1,44 +1,60 @@
+import logging
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv
+
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 class PerfectDraftConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for PerfectDraft."""
+
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is not None:
             return self.async_create_entry(title="PerfectDraft", data=user_input)
 
-        data_schema = vol.Schema({
-            vol.Required("email"): str,
-            vol.Required("password"): str,
-            vol.Required("x_api_key"): str,
-            vol.Required("recaptcha_site_key"): str,
-            vol.Required("recaptcha_secret_key"): str,
-        })
-
-        return self.async_show_form(step_id="user", data_schema=data_schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("email"): cv.string,
+                vol.Required("password"): cv.string,
+                vol.Required("x_api_key"): cv.string,
+                vol.Required("recaptcha_site_key"): cv.string,
+                vol.Required("recaptcha_secret_key"): cv.string,
+            })
+        )
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return PerfectDraftOptionsFlowHandler(config_entry)
+        return PerfectDraftOptionsFlow(config_entry)
 
-class PerfectDraftOptionsFlowHandler(config_entries.OptionsFlow):
+class PerfectDraftOptionsFlow(config_entries.OptionsFlow):
+    """Handle options flow for PerfectDraft."""
+
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
+        """Manage the options."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
         options_schema = vol.Schema({
-            vol.Optional("email", default=self.config_entry.data.get("email")): str,
-            vol.Optional("password", default=self.config_entry.data.get("password")): str,
-            vol.Optional("x_api_key", default=self.config_entry.data.get("x_api_key")): str,
-            vol.Optional("recaptcha_site_key", default=self.config_entry.data.get("recaptcha_site_key")): str,
-            vol.Optional("recaptcha_secret_key", default=self.config_entry.data.get("recaptcha_secret_key")): str,
+            vol.Required("email", default=self.config_entry.data.get("email")): cv.string,
+            vol.Required("password", default=self.config_entry.data.get("password")): cv.string,
+            vol.Required("x_api_key", default=self.config_entry.data.get("x_api_key")): cv.string,
+            vol.Required("recaptcha_site_key", default=self.config_entry.data.get("recaptcha_site_key")): cv.string,
+            vol.Required("recaptcha_secret_key", default=self.config_entry.data.get("recaptcha_secret_key")): cv.string,
         })
 
-        return self.async_show_form(step_id="init", data_schema=options_schema)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=options_schema
+        )
