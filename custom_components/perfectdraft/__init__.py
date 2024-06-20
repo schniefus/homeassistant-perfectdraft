850d4a9b1cd0caad237be10c69da14d5a6239636
matthewcky2k <matthew@luckins.im>
2024-06-20 12:02:29 +0100

    Update __init__.py

--- custom_components/perfectdraft/__init__.py
+++ custom_components/perfectdraft/__init__.py
@@ -1,6 +1,31 @@
-DOMAIN = "perfectdraft"
+import logging
+from homeassistant.helpers import discovery
+from .const import DOMAIN, PLATFORMS
 
-def setup(hass, config):
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass, config):
     """Set up the PerfectDraft component."""
+    return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up PerfectDraft from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(config_entry, platform)
+        )
+
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(config_entry.entry_id)
+
+    for platform in PLATFORMS:
+        await hass.config_entries.async_forward_entry_unload(config_entry, platform)
+
     return True
 
